604c780089a5876038bcf04f5fcbbb8600947982
Daniel Laurentino <daniellaurentino@mbp-de-daniel.home>
2017-08-28 23:02:52 -0300

    refactor: Create components from structure

---
+++ src/ListBooks.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import Bookshelf from './Bookshelf'
+import { Link } from 'react-router-dom'
+
+class ListBooks extends Component {
+    render() {
+        const bookShelves = ['Currently Reading', 'Want to Read', 'Read']
+        return (
+            <div className="list-books">
+                <div className="list-books-title">
+                    <h1>MyReads</h1>
+                </div>
+                <div className="list-books-content">
+                    <div>
+                        {bookShelves.map(shelf => (
+                            <Bookshelf title={shelf} key={shelf} />
+                        ))}
+                    </div>
+                </div>
+                <div className="open-search">
+                    <Link to="/search">Add a book</Link>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ListBooks
