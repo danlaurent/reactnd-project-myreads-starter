b31553d21dfeb9ed423f5ab9d4041ded908c7ce4
Daniel Laurentino <daniel.s.laurent@gmail.com>
2017-09-23 13:26:55 -0300

    refactor: Added return value to array-callback-return function

--- src/App.js
+++ src/App.js
@@ -17,90 +17,90 @@ class BooksApp extends React.Component {
 
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
-        let readingBooks = books.filter(b => b.shelf === 'currentlyReading')
-        let wantBooks = books.filter(b => b.shelf === 'wantToRead')
-        let readBooks = books.filter(b => b.shelf === 'read')
-        this.setState({
-            books: books,
-            currentlyReading: readingBooks,
-            wantToRead: wantBooks,
-            read: readBooks
-        })
+      let readingBooks = books.filter(b => b.shelf === 'currentlyReading')
+      let wantBooks = books.filter(b => b.shelf === 'wantToRead')
+      let readBooks = books.filter(b => b.shelf === 'read')
+      this.setState({
+        books: books,
+        currentlyReading: readingBooks,
+        wantToRead: wantBooks,
+        read: readBooks
+      })
     })
   }
 
   onShelfChange = (book, shelf) => {
     BooksAPI.update(book, shelf).then(() => {
-        book.shelf = shelf
-        let updatedBooks = []
-        const addedBook = this.state.books.filter(_ => _.id === book.id)
-        console.log(addedBook)
-        if (addedBook.length === 0) {
-          updatedBooks = this.state.currentlyReading.concat(this.state.wantToRead, this.state.read)
-          updatedBooks.push(book)
-        } else {
-          updatedBooks = this.state.currentlyReading.concat(this.state.wantToRead, this.state.read)
-        }
-        this.setState({
-            books: updatedBooks,
-            currentlyReading: updatedBooks.filter(b => b.shelf === 'currentlyReading'),
-            wantToRead: updatedBooks.filter(b => b.shelf === 'wantToRead'),
-            read: updatedBooks.filter(b => b.shelf === 'read'),
-            searchResult: []
-        })
+      book.shelf = shelf
+      let updatedBooks = []
+      const addedBook = this.state.books.filter(_ => _.id === book.id)
+      if (addedBook.length === 0) {
+        updatedBooks = this.state.currentlyReading.concat(this.state.wantToRead, this.state.read)
+        updatedBooks.push(book)
+      } else {
+        updatedBooks = this.state.currentlyReading.concat(this.state.wantToRead, this.state.read)
+      }
+      this.setState({
+        books: updatedBooks,
+        currentlyReading: updatedBooks.filter(b => b.shelf === 'currentlyReading'),
+        wantToRead: updatedBooks.filter(b => b.shelf === 'wantToRead'),
+        read: updatedBooks.filter(b => b.shelf === 'read'),
+        searchResult: []
+      })
     })
   }
 
   searchQuery = (e, query) => {
-      if (e.key === 'Enter') {
-          BooksAPI.search(query).then((books) => {
-              const shelfBooks = this.state.books
-              books.map(book => {
-                  let myBooks = shelfBooks.filter(_ => _.id === book.id)
-                  if (myBooks.length > 0) {
-                      myBooks.map(myBook => (
-                          book.shelf = myBook.shelf
-                      ))
-                  } else if (book.shelf === undefined) {
-                      book.shelf = "none"
-                  }
-              })
-              this.setState({
-                  searchResult: books
-              })
-          })
-      }
+    if (e.key === 'Enter') {
+      BooksAPI.search(query).then((books) => {
+        const shelfBooks = this.state.books
+        books.map(book => {
+          let myBooks = shelfBooks.filter(_ => _.id === book.id)
+          if (myBooks.length > 0) {
+            myBooks.map(myBook => (
+              book.shelf = myBook.shelf
+            ))
+          } else if (book.shelf === undefined) {
+            book.shelf = "none"
+          }
+          return myBooks
+        })
+        this.setState({
+          searchResult: books
+        })
+      })
+    }
   }
 
   render() {
     return (
       <div className="app">
-          <Route exact path="/" render={() => (
-            <ListBooks 
-              books={this.state.books}
-              currentlyReading={this.state.currentlyReading}
-              wantToRead={this.state.wantToRead}
-              read={this.state.read}
-              onShelfChange={this.onShelfChange}
-            />
-          )} 
+        <Route exact path="/" render={() => (
+          <ListBooks
+            books={this.state.books}
+            currentlyReading={this.state.currentlyReading}
+            wantToRead={this.state.wantToRead}
+            read={this.state.read}
+            onShelfChange={this.onShelfChange}
           />
-          <Route path="/search" render={({history}) => (
-            <SearchBooks 
-              books={this.state.books}
-              currentlyReading={this.state.currentlyReading}
-              wantToRead={this.state.wantToRead}
-              read={this.state.read}
-              searchQuery={(e, query) => {this.searchQuery(e, query)}}
-              searchResult={this.state.searchResult}
-              onShelfChange={(book, shelf) => {
-                this.onShelfChange(book, shelf)
-                history.push('/')
-                }
-              }
-            />
-          )}
+        )}
+        />
+        <Route path="/search" render={({ history }) => (
+          <SearchBooks
+            books={this.state.books}
+            currentlyReading={this.state.currentlyReading}
+            wantToRead={this.state.wantToRead}
+            read={this.state.read}
+            searchQuery={(e, query) => { this.searchQuery(e, query) }}
+            searchResult={this.state.searchResult}
+            onShelfChange={(book, shelf) => {
+              this.onShelfChange(book, shelf)
+              history.push('/')
+            }
+            }
           />
+        )}
+        />
       </div>
     )
   }
