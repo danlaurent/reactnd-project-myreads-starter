fcfc35d35133ee71c71c5d2bd65a49b2adfb3697
Daniel Laurentino <daniel.s.laurent@gmail.com>
2017-09-25 15:29:35 -0300

    refactor: Changed from stateful to stateless functional components

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -1,30 +1,23 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Book from './Book'
 import PropTypes from 'prop-types'
 
-
-class Bookshelf extends Component {
-
-  render() {
-    const books = this.props.books
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{this.props.title}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {books.map(book => (
-              <Book
-                key={book.id}
-                book={book}
-                onShelfChange={this.props.onShelfChange}
-              />
-            ))}
-          </ol>
-        </div>
-      </div>
-    )
-  }
-}
+const Bookshelf = ({ title, value, books, onShelfChange }) => (
+  <div className="bookshelf">
+    <h2 className="bookshelf-title">{title}</h2>
+    <div className="bookshelf-books">
+      <ol className="books-grid">
+        {books.map(book => (
+          <Book
+            key={book.id}
+            book={book}
+            onShelfChange={onShelfChange}
+          />
+        ))}
+      </ol>
+    </div>
+  </div>
+)
 
 Bookshelf.propTypes = {
   title: PropTypes.string.isRequired,
--- src/components/ListBooks.js
+++ src/components/ListBooks.js
@@ -1,35 +1,33 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Bookshelf from './Bookshelf'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
+const ListBooks = ({ books, currentlyReading, wantToRead, read, onShelfChange }) => {
 
-class ListBooks extends Component {
+  const bookShelves = [
+    { title: 'Currently Reading', value: 'currentlyReading', books: currentlyReading },
+    { title: 'Want to Read', value: 'wantToRead', books: wantToRead },
+    { title: 'Read', value: 'read', books: read }
+  ]
 
-  render() {
-    const bookShelves = [
-      { title: 'Currently Reading', value: 'currentlyReading', books: this.props.currentlyReading },
-      { title: 'Want to Read', value: 'wantToRead', books: this.props.wantToRead },
-      { title: 'Read', value: 'read', books: this.props.read }
-    ]
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <div>
-            {bookShelves.map(shelf => (
-              <Bookshelf title={shelf.title} value={shelf.value} key={shelf.title} books={shelf.books} onShelfChange={this.props.onShelfChange} />
-            ))}
-          </div>
-        </div>
-        <div className="open-search">
-          <Link to="/search">Add a book</Link>
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+      <div className="list-books-content">
+        <div>
+          {bookShelves.map(shelf => (
+            <Bookshelf title={shelf.title} value={shelf.value} key={shelf.title} books={shelf.books} onShelfChange={onShelfChange} />
+          ))}
         </div>
       </div>
-    )
-  }
+      <div className="open-search">
+        <Link to="/search">Add a book</Link>
+      </div>
+    </div>
+  )
 }
 
 ListBooks.propTypes = {
